5f595a629eb2a334ab8b6a25624c519742818d28
Truong Hoang Phuong <thphuong1093@gmail.com>
2023-12-12 01:04:28 +0700

    fix: list all versions

--- lib/utils.sh
+++ lib/utils.sh
@@ -17,11 +17,6 @@ fail() {
 	exit 1
 }
 
-sort_versions() {
-	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
-}
-
 n() {
 	if [ "$(command -v n)" != "n" ]; then
 		command n "$@"
