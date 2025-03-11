fc16fce72a29a389ba2cf032e8b4fee7959ed798
Truong Hoang Phuong <thphuong1093@gmail.com>
2025-03-11 16:11:03 +0700

    fix: fail when trying to installing a rolling version

--- lib/utils.sh
+++ lib/utils.sh
@@ -5,28 +5,23 @@ set -euo pipefail
 current_script_path=${BASH_SOURCE[0]}
 plugin_dir=$(dirname "$(dirname "$current_script_path")")
 
-if [ -f "$plugin_dir/.env" ]; then
-	# shellcheck source=./.env
-	source "$plugin_dir/.env"
-fi
-
 N_DOWNLOAD_PATH="$plugin_dir/n"
 
 fail() {
-	echo -e "asdf-node: $*"
-	exit 1
+    echo -e "asdf-node: $*"
+    exit 1
 }
 
 n() {
-	if [ "$(command -v n)" != "n" ]; then
-		command n "$@"
-	elif [ -n "${ASDF_NODE_N_EXECUTABLE:-}" ]; then
-		"${ASDF_NODE_N_EXECUTABLE:-}" "$@"
-	elif [ -x "$N_DOWNLOAD_PATH" ]; then
-		"$N_DOWNLOAD_PATH" "$@"
-	else
-		curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n -o "$N_DOWNLOAD_PATH"
-		chmod +x "$N_DOWNLOAD_PATH"
-		"$N_DOWNLOAD_PATH" "$@"
-	fi
+    if [ "$(command -v n)" != "n" ]; then
+        command n "$@"
+    elif [ -n "${ASDF_NODE_N_EXECUTABLE:-}" ]; then
+        "${ASDF_NODE_N_EXECUTABLE:-}" "$@"
+    elif [ -x "$N_DOWNLOAD_PATH" ]; then
+        "$N_DOWNLOAD_PATH" "$@"
+    else
+        curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n -o "$N_DOWNLOAD_PATH"
+        chmod +x "$N_DOWNLOAD_PATH"
+        "$N_DOWNLOAD_PATH" "$@"
+    fi
 }
