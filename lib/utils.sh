5c129fa6919aefef3927fd9165812fde5181274e
Truong Hoang Phuong <thphuong1093@gmail.com>
2025-03-11 16:24:35 +0700

    chore: apply lint

--- lib/utils.sh
+++ lib/utils.sh
@@ -8,20 +8,20 @@ plugin_dir=$(dirname "$(dirname "$current_script_path")")
 N_DOWNLOAD_PATH="$plugin_dir/n"
 
 fail() {
-    echo -e "asdf-node: $*"
-    exit 1
+	echo -e "asdf-node: $*"
+	exit 1
 }
 
 n() {
-    if [ "$(command -v n)" != "n" ]; then
-        command n "$@"
-    elif [ -n "${ASDF_NODE_N_EXECUTABLE:-}" ]; then
-        "${ASDF_NODE_N_EXECUTABLE:-}" "$@"
-    elif [ -x "$N_DOWNLOAD_PATH" ]; then
-        "$N_DOWNLOAD_PATH" "$@"
-    else
-        curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n -o "$N_DOWNLOAD_PATH"
-        chmod +x "$N_DOWNLOAD_PATH"
-        "$N_DOWNLOAD_PATH" "$@"
-    fi
+	if [ "$(command -v n)" != "n" ]; then
+		command n "$@"
+	elif [ -n "${ASDF_NODE_N_EXECUTABLE:-}" ]; then
+		"${ASDF_NODE_N_EXECUTABLE:-}" "$@"
+	elif [ -x "$N_DOWNLOAD_PATH" ]; then
+		"$N_DOWNLOAD_PATH" "$@"
+	else
+		curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n -o "$N_DOWNLOAD_PATH"
+		chmod +x "$N_DOWNLOAD_PATH"
+		"$N_DOWNLOAD_PATH" "$@"
+	fi
 }
